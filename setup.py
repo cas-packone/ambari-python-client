49b5583a9bd997d5d7813089674638302b53a95e
excelwang <wanghuajin999@163.com>
2019-04-19 18:49:19 +0800

    use pip install packone-remedy to install po_remedy_ambari scripts

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "packone-remedies",
+    name = "packone-remedy",
     version = "0.0.1",
     keywords = ("pip", "packone"),
     description = "Scripts to remedy vm images (of packone)",
     long_description = "Scripts to remedy vm images (of packone)",
     license = "Apache-2.0 Licence",
 
-    url = "https://github.com/cas-bigdatalab/packone-remedies",
+    url = "https://github.com/cas-bigdatalab/packone_remedy",
     author = "Excel Wang",
     author_email = "wanghj@cnic.com",
 
