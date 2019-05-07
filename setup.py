ba729b9af023184f264d276f3d3b84196b61b3da
excelwang <wanghuajin999@163.com>
2019-05-07 19:38:30 +0800

    make it compatible with lower versions of python 2.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ambari",
-    version = "0.0.11",
+    version = "0.0.12",
     keywords = ("pip", "ambari", "packone"),
     description = "Amabri python client based on ambari rest api.",
     long_description = open('README.rst').read(),
