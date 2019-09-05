138dad40ba95796fbe2b6ab28dc044f4ec8eee52
root <excelwang>
2019-09-05 18:14:21 +0800

    bump pip package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ambari",
-    version = "0.1.4",
+    version = "0.1.5",
     keywords = ("pip", "ambari", "packone"),
     description = "Amabri python client based on ambari rest api.",
     long_description = open('README.rst').read(),
